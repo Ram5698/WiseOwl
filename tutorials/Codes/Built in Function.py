182ae5393b0998f547b83726669ecf148ff1489e
Ram5678 <sairam.b@totalebizsolutions.com>
2022-08-10 19:53:38 +0530

    Vertual environment with Modules

---
+++ tutorials/Codes/Built in Function.py
@@ -0,0 +1,19 @@
+import math
+print(math.sqrt(5))
+
+from math import sqrt
+print(sqrt(3))
+
+from math import sqrt as s
+print(s(3))
+
+from datetime import datetime
+
+print(datetime.__add__(datetime.today(),1))
+
+
+import os
+print(os.name)
+
+import sys
+print(sys.version)
